32c4e38cca9759517738f7ab3f8e7a80a135a6db
ericyew <eric.ycs@gmail.com>
2017-06-29 14:23:45 +1000

    Update Add-vNICtoVM.ps1
    updated comments section

--- Add-vNICtoVM.ps1
+++ Add-vNICtoVM.ps1
@@ -1,21 +1,14 @@
 <#
 .SYNOPSIS
-  Connects to Azure and create a new virtual machine (ARM only) from an existing VHDs and add it to a resource group to a new or existing availability set
-  or move to a new or existing availability set.
+  Connects to Azure add a new vNic to an existing VM. 
 
 .DESCRIPTION
   This runbook connects to Azure and performs the following tasks :
-  	- Stops the specified virtual machine
-	- Store the virtual machine configuration
-	- Remove the virtual machine from Azure
-	- Recreate the virtual machine with existing name, existing vhd, existing nic and new/existing availability set.
+  	- Creates the vNic and assign a name
+    - Stops the specified virtual machine
+	- Add the new vNic to the VM
 	- Starts the specified virtual machine
   
-.PARAMETER AzureCredentialAssetName
-   Optional with default of "AzureCredential".
-   The name of an Automation credential asset that contains the Azure AD user credential with authorization for this subscription. 
-   To use an asset with a different name you can pass the asset name as a runbook input parameter or change the default value for the input parameter.
-
 .PARAMETER AzureSubscriptionName  
    Optional with default of "SGG-Infrastructure-Prod-SCE".  
    The name of An Automation variable asset that contains the GUID for this Azure subscription.  
@@ -30,32 +23,43 @@
    Mandatory with no default.
    The name of the resource group which contains the targeted virtual machine. 
     
-.PARAMETER CreateNewAvailabilitySet
+.PARAMETER vNetName
    Mandatory with no default.
-   Yes or No input. 
-   New or existing availability set.
+   An existing Virtual Network to attach the vNIC to.
 
-.PARAMETER AvailabilitySetName
+.PARAMETER vNetResourceGroupName
    Mandatory with no default.
-   The name for the Availability Set to create, move or add to.
-   It must be a unique name for a new availability set or existing name.
+   The name of the resource group which contains the virtual network (vNet).
+
+.PARAMETER SubnetName
+   Mandatory with no default.
+   The name of the Subnet to attach the vNic to.
+
+.PARAMETER IPAddress
+   Mandatory with no default.
+   A static IP in the subnet to be assigned to the vNic
+
+.PARAMETER PowerOnVm
+   Mandatory with no default.
+   Yes input will power on VM after adding new vNic. If more than 1 vNic is to be added, keep this blank and run the runbook a second time for the 2nd vNic.
    
 .NOTES
 	Created By: Eric Yew
-	LAST EDIT: June 22, 2017
+	LAST EDIT: June 29, 2017
 	Last Edited By: Eric Yew
+    Source: https://github.com/ericyew/AzureAutomation/blob/master/Add-vNICtoVM.ps1
 #>
 
 #Parameters input from runbook
     param (
         [Parameter(Mandatory=$false)] 
         [String] $AzureSubscriptionName = "SGG-Infrastructure-Prod-SCE",
-
-        [parameter(Mandatory=$true)] 
-        [String] $ResourceGroupName,	
-        
+       
         [parameter(Mandatory=$true)] 
         [String] $VMName,
+
+        [parameter(Mandatory=$true)] 
+        [String] $ResourceGroupName,
 
         [parameter(Mandatory=$true)] 
         [String] $vNetName,
