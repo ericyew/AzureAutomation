e27fdd1d4f1f8f0d81c4a244e36bcb68129fcf24
ericyew <eric.ycs@gmail.com>
2018-04-30 17:50:55 +1000

    Update ChangeAdd-AvailabilitySet.ps1
    Add support for managed availability set for managed disk.

--- ChangeAdd-AvailabilitySet.ps1
+++ ChangeAdd-AvailabilitySet.ps1
@@ -1,14 +1,14 @@
 <#
 .SYNOPSIS
   Connects to Azure and add an existing virtual machine (ARM only) in a resource group to a new or existing availability set
-  or move to a new or existing availability set.
+  or move to a new or existing availability set. 
 
 .DESCRIPTION
   This runbook connects to Azure and performs the following tasks :
   	- Stops the specified virtual machine
 	- Store the virtual machine configuration
 	- Remove the virtual machine from Azure
-	- Recreate the virtual machine with existing name, existing vhd, existing nic and new/existing availability set.
+	- Recreate the virtual machine with existing name, existing vhd, existing nic and new/existing/remove availability set.
 	- Starts the specified virtual machine
   
 .PARAMETER AzureCredentialAssetName
@@ -41,21 +41,21 @@
    It must be a unique name for a new availability set or existing name.
    
 .NOTES
-	Created By: Eric Yew
-	LAST EDIT: July 7, 2016
+	Created By: Eric Yew - OLIKKA
+	LAST EDIT: April 30, 2016
 	By: Eric Yew
 	SOURCE: https://github.com/ericyew/AzureAutomation/blob/master/ChangeAdd-AvailabilitySet.ps1
 #>
 
 param (
-    [Parameter(Mandatory=$true)] 
-    [String] $AzureSubscriptionName = "Enter Azure Subscription Name",
+    [Parameter(Mandatory=$false)] 
+    [String] $AzureSubscriptionName = "WCC Production EA",
 
     [parameter(Mandatory=$true)] 
     [String] $VMName,
 
 	[parameter(Mandatory=$true)] 
-    [String] $CreateNewAvailabilitySet,
+    [String] $CreateNewAvailabilitySet = "Yes/No/Remove. Default to No",
 	
 	[parameter(Mandatory=$true)] 
     [String] $AvailabilitySetName,
@@ -64,36 +64,65 @@ param (
     [String] $ResourceGroupName	
 ) 
 
-#Error Checking: Trim white space from both ends of string enter.
+# Enable Verbose logging for testing
+#    $VerbosePreference = "Continue"
+
+# Error Checking: Trim white space from both ends of string enter.
 $VMName = $VMName -replace '\s',''
-$AzureSubscriptionName = $AzureSubscriptionName -replace '\s',''
-$CreateNewAvailabilitySet = $CreateNewAvailabilitySet -replace '\s',''	
+$AzureSubscriptionName = $AzureSubscriptionName -replace '\s',''	
 $AvailabilitySetName = $AvailabilitySetName -replace '\s',''
 $ResourceGroupName = $ResourceGroupName -replace '\s',''
 
-# Getting automation assets
-$AzureCred = Get-AutomationPSCredential -Name 'PSAdmin' -ErrorAction Stop
+# Determine Create/Remove/use existing availability set
+    if($CreateNewAvailabilitySet -eq "Yes/No/Remove. Default to No")
+    {
+        $CreateNewAvailabilitySet = "No"
+    }
+    else
+    {
+        $CreateNewAvailabilitySet = $CreateNewAvailabilitySet -replace '\s',''
+    }
 
-# Connecting to Azure
-$Conn = Get-AutomationConnection -Name AzureRunAsConnection 
-Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+# Connect to Azure
+    $Conn = Get-AutomationConnection -Name AzureRunAsConnection
+    $Cert = Get-AutomationCertificate -Name 'AzureRunAsCertificate'
+    #Write-Verbose "$Conn $Cert"
+    Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Cert.Thumbprint
 
 # Getting the virtual machine
-$VM = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName -ErrorAction Stop
-$VMConfig = $VM
-$location = $vm.location
+    $VM = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName -ErrorAction Stop
+    $VMConfig = $VM
+    $location = $vm.location
 
+# Create, add or remove availability set from VM Config
 If($CreateNewAvailabilitySet -eq 'Yes' -Or $CreateNewAvailabilitySet -eq 'Y'){
-	$as = New-AzureRmAvailabilitySet `
-	    -Name $AvailabilitySetName `
-	    -ResourceGroupName $ResourceGroupName `
-	    -Location $location     
+	# Check if unmanaged disk
+    If ($VM.StorageProfile.OsDisk.ManagedDisk -eq $null)
+    {
+        $as = New-AzureRmAvailabilitySet `
+            -Name $AvailabilitySetName `
+            -ResourceGroupName $ResourceGroupName `
+            -Location $location
+    }
+    Else 
+    {
+        $as = New-AzureRmAvailabilitySet `
+            -Name $AvailabilitySetName `
+            -ResourceGroupName $ResourceGroupName `
+            -Location $location
+            -PlatformFaultDomainCount 2 `
+            -PlatformUpdateDomainCount 5 `
+            -Sku 'Aligned'  
+    } 
 }
 ElseIf ($CreateNewAvailabilitySet -eq 'No' -Or $CreateNewAvailabilitySet -eq 'N'){
 	$as = 
         Get-AzureRmAvailabilitySet `
             -ResourceGroupName $ResourceGroupName `
             -Name $AvailabilitySetName -ErrorAction Stop		
+}
+ElseIf ($CreateNewAvailabilitySet -eq 'Remove' -Or $CreateNewAvailabilitySet -eq 'R'){
+	$as = $null	
 }
 Else {
 	Write-Output $CreateNewAvailabilitySet
@@ -116,8 +145,15 @@ else
 
 # Set VM config to include new Availability Set
     $asRef = New-Object Microsoft.Azure.Management.Compute.Models.SubResource
-    $asRef.Id = $as.Id
-    $vm.AvailabilitySetReference = $asRef # To remove VM from Availability Set, set to $null
+    If ($as -eq $null)
+    {
+        $vm.AvailabilitySetReference = $null
+    }
+    else
+    {
+        $asRef.Id = $as.Id
+        $vm.AvailabilitySetReference = $asRef # To remove VM from Availability Set, set to $null
+    } 
 	
 #Reconfigure and Clean-up VM config to reflect deployment from attached disks
     $vm.StorageProfile.OSDisk.CreateOption = "Attach"
@@ -127,9 +163,9 @@ else
     $vm.OSProfile = $null
 
 #Remove the virtual machine from Azure
-Remove-AzureRmVM -VMName $VMName -ResourceGroupName $ResourceGroupName -Force -ErrorAction Stop
+    Remove-AzureRmVM -VMName $VMName -ResourceGroupName $ResourceGroupName -Force -ErrorAction Stop
 
 #Recreate the virtual machine with the new name
-New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $VM.Location -VM $VM -Verbose
+    New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $VM.Location -VM $VM -Verbose
 
 "The virtual machine has been added to availability group and started."
