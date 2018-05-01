a469ee72185a2929ee36bb76226f40eb19ad6a84
ericyew <eric.ycs@gmail.com>
2018-05-01 12:57:51 +1000

    Update Convert-UnmanagedDisk2ManagedDisk.ps1

--- Convert-UnmanagedDisk2ManagedDisk.ps1
+++ Convert-UnmanagedDisk2ManagedDisk.ps1
@@ -26,7 +26,7 @@
 
 param(
     [Parameter(Mandatory=$false)] 
-    [String] $AzureSubscriptionName = "WCC Production EA",
+    [String] $AzureSubscriptionName = "1-Prod, 2-Dev/Test, 3-Website *Defaults to Prod*",
 
     [Parameter(Position = 0, Mandatory = $true)]
     [string]
@@ -42,14 +42,99 @@ param(
 
 #Error Checking: Trim white space from both ends of string enter.
     $VMName = $VMName -replace '\s',''
-    $AzureSubscriptionName = $AzureSubscriptionName -replace '\s',''
+    $AzureSubscriptionName = $AzureSubscriptionName.trim()
     $ResourceGroupName = $ResourceGroupName -replace '\s',''
+    
+# Retrieve subscription name from variable asset if not specified
+    if($AzureSubscriptionName -eq "1" -Or $AzureSubscriptionName -eq "1-Prod, 2-Dev/Test, 3-Website *Defaults to Prod*")
+    {
+        $AzureSubscriptionName = Get-AutomationVariable -Name 'Prod Subscription Name'
+        if($AzureSubscriptionName.length -gt 0)
+        {
+            Write-Output "Specified subscription name/ID: [$AzureSubscriptionName]"
+        }
+        else
+        {
+            throw "No variable asset with name 'Prod Subscription Name' was found. Either specify an Azure subscription name or define the 'Prod Subscription Name' variable setting"
+        }
+    }
+    elseIf($AzureSubscriptionName -eq "2")
+    {
+        $AzureSubscriptionName = Get-AutomationVariable -Name 'DevTest Subscription Name'
+        if($AzureSubscriptionName.length -gt 0)
+        {
+            Write-Output "Specified subscription name/ID: [$AzureSubscriptionName]"
+        }
+        else
+        {
+            throw "No variable asset with name 'DevTest Subscription Name' was found. Either specify an Azure subscription name or define the 'DevTest Subscription Name' variable setting"
+        }
+    }
+    elseIf($AzureSubscriptionName -eq "3")
+    {
+        $AzureSubscriptionName = Get-AutomationVariable -Name 'Website Subscription Name'
+        if($AzureSubscriptionName.length -gt 0)
+        {
+            Write-Output "Specified subscription name/ID: [$AzureSubscriptionName]"
+        }
+        else
+        {
+            throw "No variable asset with name 'Website Subscription Name' was found. Either specify an Azure subscription name or define the 'Website Subscription Name' variable setting"
+        }
+    }
+    else
+    {
+        if($AzureSubscriptionName.length -gt 0)
+        {
+            Write-Output "Specified subscription name/ID: [$AzureSubscriptionName]"
+        }
+        else
+        {
+            throw "No variable asset or subscription with name $AzureSubscriptionName was found. Either specify an Azure subscription name or specify 1,2 or 3 options"
+        }
+    }
 
-#Connect to Azure
-    $Conn = Get-AutomationConnection -Name AzureRunAsConnection
-    $Cert = Get-AutomationCertificate -Name 'AzureRunAsCertificate'
-    Write-Verbose "$Conn $Cert"
-    Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Cert.Thumbprint
+# Connect to Azure
+    try
+    {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection=Get-AutomationConnection -Name AzureRunAsConnection         
+
+        "Logging in to Azure..."
+        Connect-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+    }
+    catch {
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection AzureRunAsConnection not found."
+            throw $ErrorMessage
+        } else{
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+    Select-AzureRmSubscription -SubscriptionName $AzureSubscriptionName
+
+# Check for Availability Set and Type
+    $VM = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName -ErrorAction Stop
+    $as = Get-AzureRmResource -ResourceId $VM.AvailabilitySetReference.ID
+    If($as.Sku.name -eq 'Classic')
+    {
+        $asRgName = $as.ResourceGroupName
+        $asName = $as.Name
+
+        $avSet = Get-AzureRmAvailabilitySet -ResourceGroupName $asRgName -Name $asName
+        Update-AzureRmAvailabilitySet -AvailabilitySet $avSet -Sku Aligned -ErrorVariable errorMsg -ErrorAction SilentlyContinue
+        If ($errorMsg -ne $null)
+        {
+            $avSet.PlatformFaultDomainCount = 2
+            Update-AzureRmAvailabilitySet -AvailabilitySet $avSet -Sku Aligned
+        }
+    }
 
 #Stop the VM
     "Shutting down the virtual machine ..."
